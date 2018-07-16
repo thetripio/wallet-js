f36d95a614b362b0846bdefd4ba211419bd7ebee
Jay <lijiakof@gmail.com>
2018-07-16 18:40:37 +0800

    build pkg

--- dist/index.js
+++ dist/index.js
@@ -18,65 +18,95 @@ let web3 = new Web3();
  * @module EthUtil
  */
 var Util = {
-    toWei: (num, unit) => {
-        return web3.toWei(num, unit);
-    },
-    fromWei: (num, unit) => {
-        return web3.fromWei(num, unit);
-    },
-    toBigNumber: value => {
-        return web3.toBigNumber(value);
-    },
-    toBuffer: any => {
-        return EthUtil.toBuffer(any);
-    },
-    toHex: any => {
-        return web3.toHex(any);
-    },
-    verifyPrivateKey: privateKey => {
-        let pk = EthUtil.toBuffer(privateKey);
+  /**
+   * convert unit to wei
+   * @param { Number | String | BigNumber }
+   * @param { String } uint: tether|gether|mether|kether|ether|finney|szabo|gwei|mwei|kwei
+   * @returns { String|BigNumber }
+   */
+  toWei: (num, unit) => {
+    return web3.toWei(num, unit);
+  },
+  /**
+   * convert unit from wei
+   * @param { Number | String | BigNumber }
+   * @param { String } uint: tether|gether|mether|kether|ether|finney|szabo|gwei|mwei|kwei
+   * @returns { String|BigNumber }
+   */
+  fromWei: (num, unit) => {
+    return web3.fromWei(num, unit);
+  },
+  /**
+   * convert string or number to bigNumber
+   * @param { String | Number }
+   * @returns { Object }
+   */
+  toBigNumber: value => {
+    return web3.toBigNumber(value);
+  },
+  /**
+   * convert string to buffer
+   * @param { String }
+   * @returns { Object }
+   */
+  toBuffer: any => {
+    return EthUtil.toBuffer(any);
+  },
+  /**
+   * convert string to hex
+   */
+  toHex: any => {
+    return web3.toHex(any);
+  },
+  fromHex: () => {},
+  /**
+   * verify private key
+   * @param { String } privateKey
+   * @returns { Boolean }
+   */
+  verifyPrivateKey: privateKey => {
+    let pk = EthUtil.toBuffer(privateKey);
 
-        return pk.length == 32 && Secp256k1.privateKeyVerify(pk);
-    },
+    return pk.length == 32 && Secp256k1.privateKeyVerify(pk);
+  },
+  /**
+   * encode ABI with contract
+   * @param { String } methodName
+   * @param { Array } types
+   * @param { Array } args
+   * @return { String }
+   */
+  encodeAbi: (methodName, types, args) => {
+    let methodId = Abi.methodID(methodName, types);
+    let encoded = Abi.rawEncode(types, args);
 
-    /**
-     * decode ABI with contract
-     * @param { Array } types
-     * @param { String } data
-     * @return { String }
-     */
-    decodeAbi: (types, data) => {
-        let decoded = Abi.rawDecode(types, EthUtil.toBuffer(data));
+    return '0x' + methodId.toString('hex') + encoded.toString('hex');
+  },
+  /**
+   * decode ABI with contract
+   * @param { Array } types
+   * @param { String } data
+   * @return { String }
+   */
+  decodeAbi: (types, data) => {
+    let decoded = Abi.rawDecode(types, EthUtil.toBuffer(data));
 
-        return decoded.toString('hex');
-    },
-    /**
-     * encode ABI with contract
-     * @param { String } methodName
-     * @param { Array } types
-     * @param { Array } args
-     * @return { String }
-     */
-    encodeAbi: (methodName, types, args) => {
-        let methodId = Abi.methodID(methodName, types);
-        let encoded = Abi.rawEncode(types, args);
+    return decoded.toString('hex');
+  },
+  /**
+   * 
+   * @param { Object } transactionObject
+   * @param { String } privateKey
+   * @return { String }
+   */
+  signTransaction: (transactionObject, privateKey) => {
+    let tx = new Transaction(transactionObject);
+    tx.sign(EthUtil.toBuffer(privateKey));
 
-        return '0x' + methodId.toString('hex') + encoded.toString('hex');
-    },
-    /**
-     * 
-     * @param { Object } transactionObject
-     * @param { String } privateKey
-     * @return { String }
-     */
-    signTransaction: (transactionObject, privateKey) => {
-        let tx = new Transaction(transactionObject);
-        tx.sign(EthUtil.toBuffer(privateKey));
+    let serialize = tx.serialize().toString('hex');
 
-        let serialize = tx.serialize().toString('hex');
-
-        return '0x' + serialize;
-    }
+    return '0x' + serialize;
+  }
 };
 
 var Eip20 = [{
@@ -224,7 +254,6 @@ class EthWallet {
 
     /**
      * generate wallet
-     * @return 
      */
     generate() {
         var wallet = Wallet.generate();
@@ -361,13 +390,12 @@ class EthWallet {
     /**
      * get trasaction gasLimit
      * @param { Object } transactionObject 
+     * @return { Promise }
      */
     estimateGas(transactionObject) {
         return new Promise((resolve, reject) => {
             let txObj = this._createTransaction(transactionObject);
             delete txObj._contractMethod;
-
-            console.log(txObj);
 
             web3$1.eth.estimateGas(txObj, (err, res) => {
                 if (err) {
@@ -382,6 +410,7 @@ class EthWallet {
     /**
      * send transaction
      * @param { Object } transactionObject
+     * @return { Promise }
      */
     sendTransaction(transactionObject) {
         if (!transactionObject.nonce && transactionObject.nonce !== 0) {
@@ -421,6 +450,9 @@ class EthWallet {
                     if (err) {
                         reject(err);
                     } else {
+                        console.log('_sendTransaction');
+                        console.log(contractMethod.returns);
+                        console.log(res);
                         resolve(Util.decodeAbi(contractMethod.returns, res));
                     }
                 });
@@ -509,6 +541,7 @@ class EthWallet {
 
     /**
      * get gas price
+     * @return { Promise }
      */
     gasPrice() {
         return new Promise((resolve, reject) => {
@@ -521,28 +554,6 @@ class EthWallet {
             });
         });
     }
-
-    _test() {
-
-        console.log(web3$1.eth.accounts);
-        console.log(web3$1.eth.defaultBlock);
-        console.log(web3$1.eth.blockNumber);
-        console.log(web3$1.eth.getBlock(50601));
-
-        // console.log(web3.eth.getTransactionCount(web3.eth.accounts[0]));
-
-        // var transaction = web3.eth.getTransactionFromBlock('latest', 0);
-        // console.log(transaction);
-
-        // let filter = web3.eth.filter({
-        //     address: '0xb02d5da39628918daa9545388f1abb60be368e0a'
-        // });
-
-        // filter.get((err, log) => {
-        //     console.log(log);
-        // });
-    }
-
 }
 
 //import BtcWallet from './lib/btc/btc-wallet';
